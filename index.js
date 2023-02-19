27cd6fda42696eceb389fe1b994518c2a9476d4f
Tatiana <tatiana_ivanova89@mail.ru>
2023-02-19 17:04:28 +0600

    fix issue after review

--- index.js
+++ index.js
@@ -1,47 +1,84 @@
 const EventEmitter = require('events');
 class Emitter extends EventEmitter {
     timer = (args) => {
-        const {time, id} = args;
-        this.emit('timer', {time, id});
+        const {date, id} = args;
+        this.emit('timer', {date, id});
     }
 };
 
 const emitter = new Emitter;
 
 emitter.on('timer', (args) => {
-    const {time, id} = args;
-    const total = toSec(time);
-    timer(total, id);
+    const {date, id} = args;
+    timer(date, id);
 });
 
-// функция для перевода аргументов в секунды
-const toSec = (arg) => {
-    const arr = arg.split("-");
-    const hours = +arr[0] || 0;
-    const days = +arr[1] || 0;
-    const months = +arr[2] || 0;
-    const years = +arr[3] || 0;
-    const total = 60 * 60 * (hours + 24 * (days + 30 * (months + years * 12)));
+// форматируем введенную дату
+const formatDate= (date) => {
+    const arr = date.split("-");
+    const hours = arr[0];
+    const days = arr[1];
+    const months = arr[2];
+    const years = arr[3];
+    const newDate = `${years}-${months}-${days}T${hours}:00:00.000Z`;
+    return newDate;
+};
 
-    return total;
+// вычисление остатка времени
+function getTimeRemaining(endTime) {
+    let days, hours, minutes, seconds;
+    
+    const t = Date.parse(formatDate(endTime)) - Date.parse(new Date());
+
+    if (t <= 0) {
+        days = 0;
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+    } else {
+        days = Math.floor(t/(1000 * 60 * 60 * 24)),
+        hours = Math.floor((t/(1000 * 60 * 60)) % 24),
+        minutes = Math.floor((t/(1000 * 60)) % 60),
+        seconds = Math.floor((t/1000) % 60);
+    }
+
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
 };
 
 // создание таймера
-const timer = (total, i) => {
-    let t = total;
+const timer = (date, i) => {
+
     const timeInterval = setInterval(() => {
-        if (t > 0) {
-            t--;
-            console.log(`timer ${i-2} is ${t}`);
-        } else {
+        const {total, days, hours, minutes, seconds} = getTimeRemaining(date);
+
+        console.log(`timer ${i-2}: is ${getZero(days)} days: ${getZero(hours)}:${getZero(minutes)}:${getZero(seconds)}`);
+
+        if (total <= 0) {
             console.log(`timer ${i-2} is out`);
             clearInterval(timeInterval);
         }
     }, 1000);
 };
 
+// добавляем 0
+function getZero(num) {
+    if (num >= 0 && num < 10) {
+        return `0${num}`;
+    } else {
+        return num;
+    }
+}
+
 if (process.argv.length > 2) { 
     for (let i = 3; i <= process.argv.length; i++) {
-        emitter.timer({time: process.argv[i-1], id: i});
+        emitter.timer({date: process.argv[i-1], id: i});
     }
+} else {
+    console.log('No date');
 }
