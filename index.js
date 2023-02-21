d8de1b34836563049219bc9fec5e4194c2001289
Tatiana <tatiana_ivanova89@mail.ru>
2023-02-21 18:37:48 +0600

    HW3_p1_render_list

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
-const fs = require('fs');
+#!/usr/bin/env node
 
-const readStream = fs.createReadStream('./log.txt', 'utf-8');
+const fs = require("fs");
+const http = require('http');
+const path = require("path");
 
-const ip1 = '89.123.1.41';
-const ip2 = '34.48.240.111';
+// получаем путь к директории, из которой запущена программа
+const currentDirectory = process.cwd();
 
-const writeStreamCreater = (ip, newText) => {
-    const writeStream = fs.createWriteStream(`./%${ip}%_requests.log`,  { flags: 'a', encoding: 'utf8' });
-    writeStream.write(newText);
-}
+// функция для проверки является ли аргумент файлом
+const isFile = fileName => {
+    return fs.lstatSync(fileName).isFile();
+};
 
-const findLog = (ip, chunk) => { 
-    if (chunk.search(ip) > -1) {
-        let idx = chunk.indexOf(ip);
-        let arrIp = [];
-        while (idx != -1) {
-            arrIp.push(idx);
-            idx = chunk.indexOf(ip, idx + 1);
-        };
+// функция для рендеринга разметки
+const createList = (directory) => {
+    // получаем содержимое директории
+    const list = fs.readdirSync(directory);
 
-        for (let i = 0; i < arrIp.length; i++) {
-            text = chunk.slice(arrIp[i]);
-            const newText = text.slice(0, text.search(/\n/));
-            writeStreamCreater(ip, '\n');
-            writeStreamCreater(ip, newText);
-        };
-    };
- }
+    const ul = list.map(el => {
+        // let li;
+        // if (!isFile(el)) {
+        //     li = `<li><a href="${el}">${el}</a></li>`
+        // } else {
+        //     li = `<li>${el}</li>`
+        // };
+        return `<li><a href="${el}">${el}</a></li>`;
+    });
+    return ul;
+    
+};
 
-readStream.on('data', (chunk) => {
-    findLog(ip1, chunk);
-    findLog(ip2, chunk);
+// создаем http-сервер
+const server = http.createServer((req, res) => {
+    const newPath = path.join(currentDirectory, req.url);
+    const list = createList(currentDirectory).join('');
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`<h3>Current directory:`);
+    res.write(`<h3>${newPath}</h3>`);
+    res.write(`<ul>${list}<li><a href="#">Back</a></li></ul>`);
+    res.end();
+    
 });
 
-readStream.on('end', () => console.log('File reading finished'));
-readStream.on('error', () => console.log(err));
-
-
+server.listen(3002, 'localhost', error => {
+    error ? console.log(error) : console.log('Listening');
+});
